3c1727891014a9c596fbaa43afe14fc84802b4bc
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2021-10-05 20:49:05 +0200

    Tweak load function

--- js/load.js
+++ js/load.js
@@ -1,4 +1,7 @@
 // https://stackoverflow.com/a/17901198/2773311
+// https://stackoverflow.com/a/63519229/2773311
 function load(url, element) {
-	fetch(url).then(html => { document.getElementById(element).innerHTML = html; });
+	fetch(url)
+		.then(res => { res.text(); })
+		.then(html => { document.getElementById(element).innerHTML = html; });
 }
