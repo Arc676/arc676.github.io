e7b965754515c731adf02aec7cefadec3053e170
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2021-10-05 21:01:07 +0200

    Get element in HTML

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
 // https://stackoverflow.com/a/17901198/2773311
 // https://stackoverflow.com/a/63519229/2773311
-function load(doc, url, element) {
+function load(url, element) {
 	fetch(url)
 		.then(res => { res.text(); })
-		.then(html => { doc.getElementById(element).innerHTML = html; });
+		.then(html => { element.innerHTML = html; });
 }
